9fc5d3c8b3197523636284e008c5d9b8114ced0a
FirstMateJames <firstmatekermit@gmail.com>
2025-07-08 19:25:38 +0100

    First commit

--- colour_puzzler_2d/Game/Configs/StageConfig.gd
+++ colour_puzzler_2d/Game/Configs/StageConfig.gd
@@ -14,6 +14,7 @@ static func get_stage_node(Scene : Stages) -> Node:
 	return load(GET_STAGE_NODE.get(Scene)).instantiate()
 
 
+
 const GET_MAIN_MENU = "res://Game/Levels/Menus/MainMenu.tscn"
 const GET_LEVEL_SELECT = "res://Game/Levels/Menus/Level_Select.tscn"
 
